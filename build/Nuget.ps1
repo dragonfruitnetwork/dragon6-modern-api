08ec775357149eae56cca3237b9f01e32a96ef89
Albie <admin@dragonfruit.network>
2021-02-12 10:11:43 +0000

    Update Nuget.ps1

--- build/Nuget.ps1
+++ build/Nuget.ps1
@@ -1,22 +1,20 @@
 param ([string] $ApiKey, [string]$Suffix = "")
 
-#versioning info
+# versioning info
 $VERSION = "$(Get-Date -UFormat "%Y.%m%d").$($env:GITHUB_RUN_NUMBER)$($Suffix)"
 $WORKINGDIR = Get-Location
+$TARGET = "DragonFruit.Six.Api.Modern"
 
-#Build files
-
-Write-Output "Building DragonFruit.Six.API Version $VERSION"
+# build files
+Write-Output "Building $TARGET Version $VERSION"
 dotnet restore
 dotnet build -c Release /p:PackageVersion=$VERSION
 
-#pack into nuget files with the suffix if we have one
+# pack into nuget files with the suffix if we have one
+Write-Output "Publishing $TARGET Version $VERSION"
+dotnet pack ".\$TARGET\$TARGET.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION -p:Version=$VERSION
 
-Write-Output "Publishing DragonFruit.Six.API Version $VERSION"
-dotnet pack "..\DragonFruit.Six.API\DragonFruit.Six.API.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION -p:Version=$VERSION
-
-#recursively push all nuget files created
-
+# recursively push all nuget files created
 Get-ChildItem -Path $WORKINGDIR -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
     dotnet nuget push $_ --api-key $ApiKey --source https://api.nuget.org/v3/index.json --force-english-output
 }
